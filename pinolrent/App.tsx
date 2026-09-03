98804588a334251b514f0fec8d94e307801ee4d1
Tapachula <kentoworkspace24@gmail.com>
2026-09-03 16:52:29 -0600

    feat: added metro bundler and react-dom for web development

--- pinolrent/App.tsx
+++ pinolrent/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Using Metro Bundler and Expo for Pinol Rent!</Text>
       <StatusBar style="auto" />
     </View>
   );
