91c3b369a6b75489995a3d7c1a367ef71c16898e
Tapachula <kentoworkspace24@gmail.com>
2026-09-04 18:25:05 -0600

    feat(fase-2): add cars service with list and detail

---
+++ src/services/cars.service.ts
@@ -0,0 +1,9 @@
+import api from './api'
+import type { Car, CarsListParams } from '@/types/car'
+
+export const carsService = {
+  list: (params?: CarsListParams) =>
+    api.get<Car[]>('/cars', { params }).then((r) => r.data),
+
+  get: (id: number) => api.get<Car>(`/cars/${id}`).then((r) => r.data),
+}
