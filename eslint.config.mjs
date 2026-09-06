20b4b0d7b7b398cdd00b2983f6674ac59faca321
Tapachula <kentoworkspace24@gmail.com>
2026-09-05 21:15:58 -0600

    chore: relax unescaped entities rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,10 @@
 import expo from 'eslint-config-expo/flat.js'
 
-export default [...expo]
+export default [
+  ...expo,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+]
