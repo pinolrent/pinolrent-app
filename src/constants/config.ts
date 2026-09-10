3c47a649a855991afd6236caa1fe6d3f65db1c60
Tapachula <kentoworkspace24@gmail.com>
2026-09-09 19:26:12 -0600

    chore: enforce https in prod and add eas base

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -6,3 +6,9 @@ if (!process.env.EXPO_PUBLIC_API_URL && __DEV__) {
     '[config] EXPO_PUBLIC_API_URL no definido, usando http://localhost:8080'
   )
 }
+
+if (!__DEV__ && API_URL.startsWith('http://')) {
+  throw new Error(
+    '[config] EXPO_PUBLIC_API_URL debe usar https:// en producción'
+  )
+}
